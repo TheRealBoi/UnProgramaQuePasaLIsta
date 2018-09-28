3f2e6cef088324bd9d79d4e4afa5eb4e2d9cb350
TheRealBoi <cf17marc.soler@iesjoandaustria.org>
2018-09-28 17:29:10 +0200

    Cambiado el nombre de la clase Horario a Día, para la posterior lista de Días que guarde el monton de Días para guardar los módulos

--- app/src/main/java/com/example/cf17marcsoler/passallista/MainActivity.java
+++ app/src/main/java/com/example/cf17marcsoler/passallista/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TextView hora = findViewById(R.id.textView);
-        TextView hores = findViewById(R.id.textView);
+        TextView hores = findViewById(R.id.TextView);
 
 
         EditText introduirHores = findViewById(R.id.editText);
