1b8547e92467f95849d438008c39392bef412151
TheRealBoi <cf17marc.soler@iesjoandaustria.org>
2018-09-28 16:38:12 +0200

    Primera programación de la clase Horario, es posible que tengamos que hacer la clase Modulo

--- app/src/main/java/com/example/cf17marcsoler/passallista/MainActivity.java
+++ app/src/main/java/com/example/cf17marcsoler/passallista/MainActivity.java
@@ -4,12 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     int modul;
     String parseString;
-    Horario horario = new Horario(6877);
+    Dia horario = new Dia(6877);
+    Spinner desplegable1hora;
+    Spinner desplegable2hora;
+    Spinner desplegable3hora;
+    Spinner desplegable4hora;
+    Spinner desplegable5hora;
+    Spinner desplegable6hora;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         TextView hores = findViewById(R.id.Hores);
         EditText introduirHores = findViewById(R.id.editText);
 
+        desplegable1hora.findViewById(R.id.desp1);
+        desplegable2hora.findViewById(R.id.desp2);
+        desplegable3hora.findViewById(R.id.desp3);
+        desplegable4hora.findViewById(R.id.desp4);
+        desplegable5hora.findViewById(R.id.desp5);
+        desplegable6hora.findViewById(R.id.desp6);
 
         for(int i = 0; i < horario.horas.length; i++ ){
             introduirHores.setText("Hora", TextView.BufferType.EDITABLE);
